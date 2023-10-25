fa979c276d31d4a9b632fd13f3e61579ee6c4a75
reiyanyan <hariyantoriyan.hr@gmail.com>
2023-10-25 17:29:42 +0700

    fix: og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,40 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         ></link>
+        <link rel="icon" href="/favicon.ico"></link>
+        <title>Happy Birthday</title>
+        <meta
+          name="description"
+          content="ka bima blablablabla lorem ipsum dolor sit amet, boleh diganti ini"
+        />
+
+        <meta property="og:url" content="https://klodi-hbd.vercel.app" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/image.svg" />
+        <meta
+          property="og:title"
+          content="Title apapun ini boleh diganti sih"
+        />
+        <meta
+          property="og:description"
+          content="ka bima blablablabla lorem ipsum dolor sit amet, boleh diganti ini"
+        />
+
+        <meta property="twitter:url" content="https://klodi-hbd.vercel.app" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/image.svg" />
+        <meta
+          name="twitter:title"
+          content="Title apapun ini boleh diganti sih"
+        />
+        <meta
+          name="twitter:description"
+          content="ka bima blablablabla lorem ipsum dolor sit amet, boleh diganti ini"
+        />
+
+        <meta property="twitter:domain" content="klodi-hbd.vercel.app" />
       </head>
       <body className={montserrat.className}>{children}</body>
     </html>
